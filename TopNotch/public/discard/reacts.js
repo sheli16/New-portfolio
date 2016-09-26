d3462b618d3de73cf93100b180183e962cf3fc94
sheli16 <sheli16@live.seminolestate.edu>
2016-09-25 23:24:41 -0400

    spelling, button activated, 1 google font, logo refinment, delete files and comments

--- TopNotch/public/discard/reacts.js
+++
@@ -1,9 +0,0 @@
-var CommentList = React.createClass({
-  render: function() {
-    return (
-      <div className="commentList">
-        Hello, world! I am a CommentList.
-      </div>
-    );
-  }
-});
